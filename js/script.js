e15ed7b8d847ba83f683bfad7ee317612fb0e866
SANDY LONG <xuanxuan@MacBook-Pro.local>
2020-12-01 16:56:14 -0500

    added team page title & fixed breadcrumb bug

--- js/script.js
+++ js/script.js
@@ -60,7 +60,7 @@
 		$('.a-pagepiling').pagepiling({
 			scrollingSpeed: 280,
 			menu: '#menu, #menuMain',
-			anchors: ['About', 'Services', 'Method', 'Team', 'Portfolio', 'Clients','Testimonials',  'Contact'],
+			anchors: ['About', 'Services', 'Method', 'Team', 'Portfolio', 'Partners','Testimonials',  'Contact'],
 			loopTop: false,
 			loopBottom: false,
 			navigation: {
